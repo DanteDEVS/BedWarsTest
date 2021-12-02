08633af0fa3aeebc71263afaccffc09d6bd51ee6
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-02 19:41:58 +0200

    help with fireball... more code comming right up

--- src/test/bw/tests/fireball.php
+++ src/test/bw/tests/fireball.php
@@ -2,6 +2,40 @@
 
 namespace test\bw\tests
 
-use pocketmine\entity\entity
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\entity\projectile\{Projectile, Throwable};
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\item\{ItemFactory, Item};
+use pocketmine\level\{Position, Level};
+use pocketmine\level\particle\HugeExplodeParticle;
+use pocketmine\event\entity\ExplosionPrimeEvent;
+use pocketmine\level\Explosion;
+use pocketmine\nbt\tag\ShortTag;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\math\RayTraceResult;
+use pocketmine\math\Vector3;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\Player;
 
 class Fireball extends Throwable {
+    
+    public const NETWORK_ID = self::SMALL_FIREBALL;
+
+    public $width = 0.50;
+
+    public $height = 0.50;
+    
+    protected $gravity = 0.0;
+    
+    protected $drag = 0;
+    
+    protected $life = 0;
+    
+    protected $damage = 2;
